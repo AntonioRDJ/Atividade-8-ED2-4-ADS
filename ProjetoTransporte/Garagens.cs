8082b5e9090df7c9cc8fe5241f752d77375ac2ef
Antonio Junior <antonio.rodrdj@gmail.com>
2022-01-13 20:25:14 -0300

    first-commit

---
+++ ProjetoTransporte/Garagens.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTransporte
+{
+    class Garagens
+    {
+        private List<Garagem> garagens;
+        private bool jornadaAtiva;
+
+        public List<Garagem> Garagensl
+        {
+            get { return garagens; }
+            set { garagens = value; }
+        }
+
+        public void incluir(Garagem garagem)
+        {
+            garagem.Id = garagens.Count + 1;
+            garagens.Add(garagem);
+        }
+
+        public void iniciarJornada()
+        {
+            jornadaAtiva = true;
+
+        }
+
+        public List<Transporte> encerrarJornada(List<Transporte> lTrans)
+        {
+            jornadaAtiva = false;
+
+            return lTrans;
+        }
+    }
+}
---
+++ ProjetoTransporte/Veiculos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTransporte
+{
+    class Veiculos
+    {
+        private List<Veiculo> veiculos = new List<Veiculo>();
+
+        public List<Veiculo> Veiculosl
+        {
+            get { return veiculos; }
+            set { veiculos = value; }
+        }
+
+        public void incluir(Veiculo vei)
+        {
+            vei.Id = veiculos.Count + 1;
+            veiculos.Add(vei);
+        }
+
+    }
+}
